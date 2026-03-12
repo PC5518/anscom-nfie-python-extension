010e2e46237058a4e3b5961a17f09f34699c1e40
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-03-13 04:19:35 +0530

    Update setup.py for platform-specific optimizations
    Added platform-specific compiler optimization flags and error handling for README file.

--- setup.py
+++ setup.py
@@ -1,25 +1,39 @@
+import sys
 from setuptools import setup, Extension
 
 # Read the long description from README
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A fast native C recursive file scanner and analyzer"
+
+# Detect platform-specific compiler optimization and linking flags
+if sys.platform == "win32":
+    # MSVC optimizations and strict warnings
+    extra_compile_args =['/O2', '/W4']
+    extra_link_args =[]
+else:
+    # Aggressive POSIX optimizations (GCC/Clang)
+    extra_compile_args =['-O3', '-march=native', '-Wall', '-Wextra', '-Wno-unused-parameter']
+    extra_link_args = ['-pthread']
 
 module = Extension(
     'anscom',
     sources=['anscom.c'],
-    # Optional: Compile arguments can be added here if needed
-    # extra_compile_args = ['-O3'] 
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
-    name='anscom',  # Check PyPI if this name is taken!
-    version='0.6.0',
+    name='anscom', 
+    version='1.0.0',
     author='Aditya Narayan Singh',
     author_email='adityansdsdc@outlook.com',
     description='A fast native C recursive file scanner and analyzer',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/PC5518', # Optional
+    url='https://github.com/PC5518', 
     ext_modules=[module],
     classifiers=[
         "Programming Language :: Python :: 3",
